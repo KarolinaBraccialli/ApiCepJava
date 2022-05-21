89f37c9f50d62903c41e5a60c5906a9d4853b121
Karolina <karolinabraccialli@gmail.com>
2022-05-21 11:21:14 -0300

    Configuração do repositório

---
+++ src/main/java/com/padroesprojeto/ApiCepJava/ApiCepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.padroesprojeto.ApiCepJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCepJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCepJavaApplication.class, args);
+	}
+
+}
